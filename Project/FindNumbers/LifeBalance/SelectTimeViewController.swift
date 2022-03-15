02d27404a886e883b4758ef37c696a0ab12c9338
abrosov-sergey <90787879+abrosov-sergey@users.noreply.github.com>
2022-03-15 16:42:27 +0300

    Add files via upload

---
+++ Project/FindNumbers/LifeBalance/SelectTimeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SelectTimeViewController.swift
+//  LifeBalance
+//
+//  Created by Сергей Абросов on 15.03.2022.
+//
+
+import UIKit
+
+class SelectTimeViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
---
+++ Project/FindNumbers/LifeBalance/SettingsTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsTableViewController.swift
+//  LifeBalance
+//
+//  Created by Сергей Абросов on 15.03.2022.
+//
+
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
